7f40abc768de3975121d141836704ddd8108b33a
pasadi <parna80as@gmail.com>
2022-08-27 17:28:45 +0430

    add new api in service

--- nodejs-mysql/nodejs-mysql/app.js
+++ nodejs-mysql/nodejs-mysql/app.js
@@ -1,18 +1,98 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
 
-const indexRouter = require('./routes/index');
-const quotesRouter = require('./routes/quotes');
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
+var collection = "components";
+var imgcollection = "posters";
+var database = "bartarha";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db(database);
+  var myobj = { _id: "order", queue: [] };
+  dbo.collection(collection).insertOne(myobj, function (err, res) {
+    if (err) console.log("error in creating db.");
+    db.close();
+  });
+});
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db(database);
+  var myobj = {};
+  dbo.collection(imgcollection).insertOne(myobj, function (err, res) {
+    if (err) console.log("error in creating db.");
+    db.close();
+  });
+});
 
 const app = express();
-
-app.use(logger('dev'));
+const port = 8081;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/quotes', quotesRouter);
+app.get("/orders", (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(database);
+    var query = { _id: "order" };
+    dbo
+      .collection(collection)
+      .find(query)
+      .toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result[0].queue);
+        db.close();
+      });
+  });
+});
 
-module.exports = app;
+app.post("/orders", (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(database);
+    var myquery = { _id: "order" };
+    var newvalues = { $set: { queue: req.body } };
+    dbo
+      .collection(collection)
+      .updateOne(myquery, newvalues, function (err, func) {
+        if (err) throw err;
+        res.send(200);
+        db.close();
+      });
+  });
+});
+
+app.post("/add/poster", (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(database);
+    var newvalues = { name: req.body.name, hash: req.body.img };
+    dbo.collection(imgcollection).insertOne(newvalues, function (err, func) {
+      if (err) throw err;
+      res.send(200);
+      db.close();
+    });
+  });
+});
+
+app.get("/get/poster", (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db(database);
+    if (req.query.name) var query = { name: req.query.name };
+    else var query = null;
+    dbo
+      .collection(imgcollection)
+      .find(query)
+      .toArray(function (err, result) {
+        if (err) throw err;
+        res.send(result.filter((item) => item.hash?.length > 0));
+        db.close();
+      });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
